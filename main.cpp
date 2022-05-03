0d1229d8eac6da9cebfbd24ebee52d880dc85a26
Paulanerus <reallucycode@gmail.com>
2022-05-03 09:59:26 +0200

    Implemnted template print function for std::vector

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
+template<typename T>
+void print_vec(const std::vector<T>& v)
+{
+	for (auto& i: v)
+	{
+		std::cout << i << std::endl;
+	}
+}
+
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	std::vector<int> v = { 10, 7, 9, 8, 1, 2, 3, 4, 5, 6 };
+
+	std::vector<std::string> v2 = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j" };
+
+	print_vec(v);
+
+	//Sort
+
+	print_vec(v2);
+
 	return 0;
 }
