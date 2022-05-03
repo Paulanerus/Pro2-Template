2e32a48a4f6b498e6306b339450d32ae666a8632
Paul <reallucycode@gmail.com>
2022-05-03 20:16:55 +0200

    Added exception

---
+++ StudentFileNotFoundException.h
@@ -0,0 +1,8 @@
+#ifndef PRO2_U_5_STUDENTFILENOTFOUNDEXCEPTION_H
+#define PRO2_U_5_STUDENTFILENOTFOUNDEXCEPTION_H
+
+#include <exception>
+
+class StudentFileNotFoundException : public std::exception{};
+
+#endif
