99367cac3c7037bc585342b31cdbffa469624817
Paul <reallucycode@gmail.com>
2022-05-03 10:37:57 +0200

    Implemented template selection function

--- main.cpp
+++ main.cpp
@@ -7,21 +7,48 @@ void print_vec(const std::vector<T>& v)
 {
 	for (auto& i: v)
 	{
-		std::cout << i << std::endl;
+		std::cout << i << " ";
 	}
+
+	std::cout << std::endl;
 }
 
+template<typename T>
+void simple_sort(std::vector<T>& v)
+{
+	for (size_t i = 0; i < v.size() - 1; i++)
+	{
+		size_t min = i;
+		for (size_t j = i + 1; j < v.size(); j++)
+		{
+			if (v.at(j) < v.at(min))
+			{
+				min = j;
+			}
+		}
+
+		T temp = v[i];
+
+		v[i] = v[min];
+
+		v[min] = temp;
+	}
+}
 
 int main()
 {
 	std::vector<int> v = { 10, 7, 9, 8, 1, 2, 3, 4, 5, 6 };
 
-	std::vector<std::string> v2 = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j" };
-
+	std::vector<std::string> v2 = { "Bali", "Jonas", "Ingo", "Adam", "Detlef", "Ewald", "Carl", "Georg", "Harald",
+									"Floarian" };
+	print_vec(v);
+	simple_sort(v);
 	print_vec(v);
 
-	//Sort
+	std::cout << "=================================================" << std::endl;
 
+	print_vec(v2);
+	simple_sort(v2);
 	print_vec(v2);
 
 	return 0;
